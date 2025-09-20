2c611c9b04a1d4cf06df681c060958f6fdbd9f4f
DevRuanSantiago <ruangon2021@gmail.com>
2025-09-20 17:40:26 -0300

    ADD JS AND HTML .14
    Aula sobre switch,case,default e suas propriedas

---
+++ aula14.js
@@ -0,0 +1,18 @@
+let colocação = 1;
+
+switch (colocação) {
+    case 1:
+        console.log("primeiro lugar")
+        break
+
+    case 2:
+        console.log("colocação 2 lugar")
+        break
+    case 3: case 4: case 5:
+        console.log("colocação 2 lugar")
+        break
+
+    default:
+        console.log("não subiu ao podio")
+        break
+}
