14541491e28a6754d6238b79a5020622ae75857a
DevRuanSantiago <ruangon2021@gmail.com>
2025-09-23 03:11:30 -0300

    add js and html .25
    aula sobre Arrow functions

---
+++ aula25.js
@@ -0,0 +1,19 @@
+/*const soma = (v1, v2) => {
+    return v1 + v2;
+
+}
+*/
+const nome = (n) => { return n }
+const add = (n) => n + 10;
+const retira = (n1, n2) => n1 - n2;
+
+const soma = (v1, v2) => {
+    let res = v1 + v2
+    return res
+}
+
+
+console.log(soma(15, 40))
+console.log(nome("bruno"))
+console.log(add(15))
+console.log(retira(10, 15))
