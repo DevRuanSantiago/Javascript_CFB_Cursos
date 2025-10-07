8578f19443c519961cd7c2cb513b19098115df51
Ruan gonçalves santiago <123724861+estatisticoo@users.noreply.github.com>
2025-10-07 19:52:28 -0300

    Add 37 html e css
    Aula sobre stoppropagation

---
+++ aula37.js
@@ -0,0 +1,19 @@
+const caixa1 = document.querySelector("#caixa1");
+const btn_c1 = document.querySelector("#c1");
+const coleçaoCurs=[...document.querySelectorAll(".curso")]
+caixa1.addEventListener("click",(evt)=>{
+    
+console.log(evt.target)})
+
+btn_c1.addEventListener("click",(evt)=>{
+    evt.stopPropagation()
+    console.log("clicou c1") 
+   
+})
+
+coleçaoCurs.map((ele)=>{
+    ele.addEventListener("clcik",(evt)=>{
+        evt.stopPropagation()
+    })
+
+})
