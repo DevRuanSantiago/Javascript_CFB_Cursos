da5358a38e03f802463896c8b49fe59cf1de8c54
DevRuanSantiago <ruangon2021@gmail.com>
2025-10-10 02:46:31 -0300

    Altera css e js.40
    Aula sobre removechild e manipulação de dom

--- aula40.js
+++ aula40.js
@@ -10,5 +10,14 @@ Cursos.map((ele, chave) => {
     novoelement.setAttribute("id", "c" + chave);
     novoelement.setAttribute("class", "curso c1");
     novoelement.innerHTML = ele;
+
+    const btn_lixeira = document.createElement("Img")
+    btn_lixeira.setAttribute("src", "https://cdn-icons-png.flaticon.com/512/73/73806.png")
+    btn_lixeira.setAttribute("class", "btn_lixeira")
+    novoelement.appendChild(btn_lixeira)
+    btn_lixeira.addEventListener("click", (evt) => {
+        caixa1.removeChild(evt.target.parentNode)
+    })
     caixa1.appendChild(novoelement)
+
 })
