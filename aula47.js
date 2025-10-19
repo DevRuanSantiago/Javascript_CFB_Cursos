75a0c74edfbcff18467f6de6be7eb7697c8e8397
DevRuanSantiago <ruangon2021@gmail.com>
2025-10-19 02:00:08 -0300

    Add aula 47.html .css .js

---
+++ aula47.js
@@ -0,0 +1,26 @@
+const p_array = document.getElementById("array")
+const p_pesquisar = document.getElementById("txt_pesquisar")
+const p_BtnPesquisar = document.getElementById("btnPesquisar")
+const p_resultado = document.getElementById("resultado")
+
+const elementos_array = ["html", "css", "javascript"]
+p_array.innerHTML = "[" + elementos_array + "]"
+
+
+
+
+
+p_BtnPesquisar.addEventListener("click", (evt) => {
+    const ret = elementos_array.find((e, i) => {
+        p_resultado.innerHTML = "Valor não encontrado"
+
+        if (e.toUpperCase() == p_pesquisar.value.toUpperCase()) {
+            p_resultado.innerHTML = "Valor pesquisado == " + e.toUpperCase() + " Na posição " + i
+            return e
+        }
+
+    })
+
+
+    console.log(ret)
+})
